b3b004c9c86c369d94a3147f71a90bbc9c18ba5c
harizm837-dot <harizm837@gmail.com>
2026-02-21 22:26:19 +0800

    Update economyEngine.js

--- js/economyEngine.js
+++ js/economyEngine.js
@@ -1,22 +1,34 @@
-// economyEngine.js - Эдийн засаг
+// economyEngine.js - Эдийн засгийн систем
 const EconomyEngine = {
     getWallet: function() {
         return parseInt(localStorage.getItem('hola_wallet')) || 1500;
     },
-    
+
     updateWallet: function(amount) {
         let current = this.getWallet();
         let updated = current + amount;
-        if (updated < 0) return false; // Мөнгө хүрэхгүй бол
+        if (updated < 0) return false; 
         
         localStorage.setItem('hola_wallet', updated);
-        EventBus.emit('WALLET_UPDATED', { balance: updated });
+        // app.html-рүү мэдээлэх
+        if(window.parent) window.parent.postMessage({ type: 'WALLET_UPDATED', balance: updated }, '*');
         return true;
     },
-    
+
+    // Унаа худалдаж авах (7, 14, 30 хоногоор)
+    buyVehicle: function(vehicleType, price, days) {
+        if(this.updateWallet(-price)) {
+            const expireDate = Date.now() + (days * 24 * 60 * 60 * 1000);
+            const vehicleData = { type: vehicleType, expire: expireDate };
+            localStorage.setItem('user_vehicle', JSON.stringify(vehicleData));
+            return true;
+        }
+        return false;
+    },
+
     processGift: function(giftData) {
         if (this.updateWallet(-giftData.price)) {
-            EventBus.emit('GIFT_SENT', giftData);
+            if(window.parent) window.parent.postMessage({ type: 'GIFT_SENT', ...giftData }, '*');
             return true;
         }
         return false;
